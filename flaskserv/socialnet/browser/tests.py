7999f2c1e78628a958aaded7762376a0ba84adbe
amovfx <amo.vfx@protonmail.com>
2021-08-28 12:00:26 -0600

    Added comments

--- flaskserv/socialnet/browser/tests.py
+++ flaskserv/socialnet/browser/tests.py
@@ -3,11 +3,9 @@ import random
 from flaskserv.socialnet.test_base import TestBaseCase
 from flaskserv.socialnet.browser.form import TribeForm, PostForm
 
-from flaskserv.socialnet import db
+
 from flaskserv.socialnet.data.create_db import generate_tribes
 from flaskserv.socialnet.models import User, Tribe, Post
-import lorem
-
 
 
 
@@ -19,6 +17,12 @@ class TestBrowser(TestBaseCase):
     """
 
     def setUp(self):
+        """
+
+        For setup we generate some tribes, keep the amount to make sure
+        the db is working correctly.
+
+        """
         super().setUp()
         print()
         self.tribe_count = 10
@@ -26,6 +30,11 @@ class TestBrowser(TestBaseCase):
 
 
     def test_tribe_form(self):
+        """
+
+        Test validation of the tribe form.
+
+        """
         tribe_form = TribeForm(name="Test",
                                description="Lame ass description")
 
@@ -33,16 +42,33 @@ class TestBrowser(TestBaseCase):
 
 
     def test_post_form(self):
+        """
+
+        Test validation of the post form.
+
+        """
         post_form = PostForm(title="The title",
                              message="The message")
 
         self.assertTrue(post_form.validate())
 
+
     def test_tribe_count(self):
+        """
+
+        Test that the database is working from setUp.
+
+        """
         tribe_count = len(Tribe.query.all())
         self.assertEqual(self.tribe_count, tribe_count)
 
+
     def test_tribe_page(self):
+        """
+
+        Test the tribes page.
+
+        """
 
         response = self.client.get('/tribes/new',
                                     content_type='html/text')

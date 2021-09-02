19bd15819398f45a6cf33c8f911a391e01425dd2
amovfx <amo.vfx@protonmail.com>
2021-09-02 15:25:54 -0600

    Added comment reply form.

--- flaskserv/socialnet/tribes/form.py
+++ flaskserv/socialnet/tribes/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, HiddenField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class TribeForm(FlaskForm):
@@ -21,5 +21,20 @@ class PostForm(FlaskForm):
     """
 
     title = StringField('title', validators=[DataRequired()])
-    message = StringField('message', validators=[DataRequired(), Length(min=6, max=10000)])
+    message = StringField('message', validators=[DataRequired(), Length(min=2, max=10000)])
+
+
+class CommentReplyForm(FlaskForm):
+    """
+
+    Form for replying to comments.
+
+    """
+    parent_uuid = HiddenField('parent_uuid', validators=[DataRequired()])
+    reply = StringField('reply',validators=[DataRequired(),Length(min=6, max=10000)])
+    submit = SubmitField('Submit')
+
+
+
+
 

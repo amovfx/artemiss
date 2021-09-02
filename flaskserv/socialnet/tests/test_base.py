98dec08cf6f0c7aefb1f8dd1a2cb7563e9e03db2
amovfx <amo.vfx@protonmail.com>
2021-09-02 14:48:34 -0600

    Added comment strings.

--- flaskserv/socialnet/tests/test_base.py
+++ flaskserv/socialnet/tests/test_base.py
@@ -1,7 +1,13 @@
+"""
+
+Base test case that sets up flask app and populates the database
+with synthetic users.
+
+"""
+
 from flask_testing import TestCase
 from flaskserv.socialnet import create_app, db
 from flaskserv.socialnet.config import TestConfig
-from flaskserv.socialnet.models import User
 
 from flaskserv.socialnet.data.create_db import generate_users
 
@@ -9,17 +15,38 @@ from flaskserv.socialnet.data.create_db import generate_users
 
 
 class TestBaseCase(TestCase):
+    """
+
+    Sets up db and app.
+
+    """
 
     def create_app(self):
+        """
+
+        Create an app with the testing config.
+        :return:
+            A flask app
+        """
         return create_app(config_class=TestConfig)
 
     def setUp(self) -> None:
+        """
+
+        Create SQL tables. Generate users.
+
+        :return:
+        """
         db.create_all(app=self.app)
         generate_users()
 
 
     def tearDown(self):
+        """
+
+        Tear down the database.
+
+        :return:
+        """
         db.session.remove()
         db.drop_all(app=self.app)
-
-

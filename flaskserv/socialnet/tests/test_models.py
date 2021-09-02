a3d21ca57bd833bc40b5ac5e7933809e49f5b3c0
amovfx <amo.vfx@protonmail.com>
2021-09-02 15:13:12 -0600

    Fixed up pylinting problems.

--- flaskserv/socialnet/tests/test_models.py
+++ flaskserv/socialnet/tests/test_models.py
@@ -1,129 +1,75 @@
-import random
+"""
+
+Testing model integrations.
+
+"""
 
 
-from flaskserv.socialnet.models import Tribe, User, Post
+from flaskserv.socialnet.models import Tribe, User
 from flaskserv.socialnet.tests.test_base import TestBaseCase
+from flaskserv.socialnet.data.create_db import generate_random_post
 from flaskserv.socialnet import db
 
-import lorem
-import names
-
-def get_random_item(Model):
-    """
-
-    Return a random entry of the argument model.
-    :param Model:
-    :return:
-    """
-    return random.choice(Model.query.all())
-
-def create_random_user():
-    """
-
-    Generates a random user for testing.
-
-    :return:
-        A user model.
-    """
-    name = names.get_first_name()
-    return User(name=name,
-                email=f"{name}@example.com",
-                password="testing_password")
-
-
-
-
-def generate_comment_tree(tribe,
-                          parent_post=None,
-                          depth = 3,
-                          count = 0):
-    """
-
-    Generate a comment tree
-
-    :param tribe:
-        The tribe the comments belong to.
-    :param parent_post:
-        The parent post
-    :param count:
-        parameter to keep track of depth.
-    :return:
-    """
-
-    if count < depth:
-        range_min = 2 * (2 - count)
-        range_max = 5 * (2 - count)
-
-        for _ in range(random.randint(range_min, range_max)):
-            print("Creating ")
-            rand_user = get_random_item(User)
-            child_post = Post(title=lorem.sentence().split(" ")[0],
-                            message=lorem.sentence(),
-                            owner=rand_user,
-                            tribe=tribe)
-
-            if parent_post:
-                child_post.parent = parent_post
-
-            db.session.add(child_post)
-
-            if random.uniform(0,1) < .2:
-                generate_comment_tree(tribe,
-                                      parent_post=child_post,
-                                      count=count + 1)
-
-
-    db.session.commit()
-    return None
 
 class TestIntegrations(TestBaseCase):
+    """
+
+    Testing database.
+
+    """
 
     def setUp(self):
+        """
+
+        Create testing data, overloaded the base function.
+        :return:
+        """
+
         user = User()
         db.session.add(user)
 
         tribe = Tribe(name="Test",description="Best Tribe")
         tribe.owner_id = user.id
-        tribe.owner = user
+        tribe.tribe_owner = user
         db.session.add(tribe)
 
-        post = Post(title=lorem.sentence().split("0")[0],
-                    message=lorem.sentence(),
-                    owner=user,
-                    tribe=tribe)
+        post = generate_random_post(tribe, user)
 
         db.session.add(post)
 
         for _ in range(5):
-            child_post = Post(title=lorem.sentence().split(" ")[0],
-                        message=lorem.sentence(),
-                        owner=user,
-                        parent=post,
-                        tribe=tribe)
-
+            child_post = generate_random_post(tribe, user)
             db.session.add(child_post)
-
-
-        tribe_tree = Tribe(name="Tree Test",description="Best Tribe")
-        tribe_tree.owner_id = user.id
-        tribe_tree.owner = user
-        db.session.add(tribe_tree)
-
-        generate_comment_tree(tribe_tree)
 
         db.session.commit()
 
 
 
     def test_get_user(self):
+        """
+
+        Testing getting a user.
+
+        :return:
+        """
         user = User.query.filter_by(name="TestUser").first()
         self.assertTrue((user is not None))
 
     def test_get_tribe(self):
+        """
+
+        Testing getting a tribe.
+        :return:
+        """
         tribe = User.query.filter_by(name="Test")
         self.assertTrue((tribe is not None))
 
     def test_get_post_from_user(self):
+        """
+
+        Testing getting a post from a user.
+
+        """
         user = User.query.filter_by(name="TestUser").first()
         for post in user.posts:
             print(f"title: {post.title}")
@@ -131,19 +77,13 @@ class TestIntegrations(TestBaseCase):
         self.assertGreaterEqual( len(user.posts),1)
 
     def test_get_post_from_tribe(self):
+        """
+
+        Testing getting a post from a tribe.
+
+        """
         tribe = Tribe.query.filter_by(name="Test").first()
         for post in tribe.posts:
             print(f"title: {post.title}")
             print(f"message: {post.message}\n")
         self.assertEqual(6, len(tribe.posts))
-
-    def test_post_tree(self):
-        tribe = Tribe.query.filter_by(name="Test").first()
-
-
-
-
-
-
-
-

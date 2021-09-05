697fec57c952b643af08b5df38d303fc7a023680
amovfx <amo.vfx@protonmail.com>
2021-09-05 16:35:16 -0600

    Adding blueprint template python files.

---
+++ flaskserv/socialnet/comments/form.py
@@ -0,0 +1,9 @@
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, HiddenField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class CommentsForm(FlaskForm):
+    pass
+   
+    
---
+++ flaskserv/socialnet/comments/views.py
@@ -0,0 +1,23 @@
+
+
+import json
+
+from flask import (Blueprint,
+                   request,
+                   redirect,
+                   url_for,
+                   render_template,
+                   make_response,
+                   jsonify)
+
+from flask_login import login_required, current_user
+from flaskserv.socialnet import db
+
+from flaskserv.socialnet.comments.form import CommentsForm
+
+
+comments_bp = Blueprint('name',
+                      __name__,
+                      template_folder='templates',
+                      static_url_path='/comments/static',
+                      static_folder='static')

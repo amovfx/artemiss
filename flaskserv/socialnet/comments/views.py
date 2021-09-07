9aa22e582419537bbe0649812817c36bba0b68c1
amovfx <amo.vfx@protonmail.com>
2021-09-07 17:29:26 -0600

    named blueprint properly

--- flaskserv/socialnet/comments/views.py
+++ flaskserv/socialnet/comments/views.py
@@ -13,49 +13,11 @@ from flask import (Blueprint,
 from flask_login import login_required, current_user
 from flaskserv.socialnet import db
 
-from flaskserv.socialnet.models import Post, User
-
 from flaskserv.socialnet.comments.form import CommentsForm
 
 
-comments_bp = Blueprint('name',
+comments_bp = Blueprint('comments',
                       __name__,
                       template_folder='templates',
                       static_url_path='/comments/static',
                       static_folder='static')
-
-
-@comments_bp.get('/comments/<tribe>')
-@login_required
-def get_comments(tribe):
-    """
-
-    Gets the comments of a tribe.
-    This should be a generic function.
-
-    :param tribe:
-        The tribe of the comments.
-    :return:
-        json of the orms comments.
-    """
-
-
-    counter = int(request.args.get("c"))
-
-    page = db.session.query(Post, User) \
-        .filter(Post.author_id == User.id) \
-        .filter(Post.tribe_id == tribe) \
-        .paginate(counter, 10, False)
-
-
-    data = []
-    for post, user in page.items:
-        data.append(post.preview(user))
-
-
-    return jsonify(data), 200
-
-
-
-
-

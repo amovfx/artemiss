1bf949ec148e793e8e1d74e7fff8e1805605b13a
amovfx <amo.vfx@protonmail.com>
2021-09-08 14:46:04 -0600

    removed use for a global variable.

--- flaskserv/socialnet/comments/views.py
+++ flaskserv/socialnet/comments/views.py
@@ -31,35 +31,37 @@ def get_comments(tribe):
 
     PAGE_COUNT = 15
 
-    def get_tribe_posts():
+    def get_tribe_posts(tribe):
         tribe_posts = db.session.query(Post, User).filter(Post.tribe_id == tribe)
         return tribe_posts
 
     def get_comment_page(tribe_posts, page, quantity):
-        page = tribe_posts.filter(Post.author_id == User.id).paginate(page, quantity, False)
+        page_query = tribe_posts.filter(Post.author_id == User.id).paginate(page, quantity, False)
+
         data = []
-        for post, user in page.items:
+        for post, user in page_query.items:
             data.append(post.preview(user))
-        return jsonify(data)
+
+        return make_response(jsonify(data), 200)
 
 
     if request.args:
         counter = int(request.args.get("c"))
 
-        tribe_posts_query = get_tribe_posts()
+        tribe_posts_query = get_tribe_posts(tribe)
 
         if counter == 0:
-            response = make_response(get_comment_page(tribe_posts_query,
-                                                              page=0,
-                                                              quantity=PAGE_COUNT), 200)
+            response = get_comment_page(tribe_posts_query,
+                                        page=0,
+                                        quantity=PAGE_COUNT)
 
         elif counter == tribe_posts_query.count():
             response = make_response(jsonify({}), 200)
 
         else:
-            response = make_response(get_comment_page(tribe_posts_query,
-                                                              page=counter,
-                                                              quantity=PAGE_COUNT), 200)
+            response = get_comment_page(tribe_posts_query,
+                                        page=counter,
+                                        quantity=PAGE_COUNT)
 
         return response
 
